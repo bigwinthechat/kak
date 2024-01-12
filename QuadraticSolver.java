d0d11522a34b1e337be753e163653638062a1f08
Warren Peng <wpeng26@students.polytechnic.org>
2024-01-12 14:36:20 -0800

    hi

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,5 +1,7 @@
 public class QuadraticSolver{
     public static void main(String[] args){
         System.out.println("Welcome to the Quadratic solver");
+        System.out.println("hola");
+        System.out.println("The quadratic formuala is not complicated");
     }
 }
